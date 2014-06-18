6f3984357e66c1c8796ad8d90a6e2a6fa11d712d
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-06-18 18:48:48 +0530

    Build libusb submodule
    1. libusb-1.0.9-android.patch is not needed because this patch is
    already included in libusb submodule
    2. no udev on android, hence --disable-udev
    3. Other build instructions
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- build.sh
+++ build.sh
@@ -112,20 +112,16 @@ if [ ! -e $PKG_CONFIG_PATH/libgit2.pc ] ; then
 	popd
 fi
 
-if [ ! -e libusb-1.0.9.tar.bz2 ] ; then
-	wget http://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-1.0.9/libusb-1.0.9.tar.bz2
+if [ ! -e libusb/configure ] ; then
+	pushd libusb
+	autoreconf --install
+	popd
 fi
-if [ ! -e libusb-1.0.9 ] ; then
-	tar -jxf libusb-1.0.9.tar.bz2
-fi
-if ! grep -q __ANDROID__ libusb-1.0.9/libusb/io.c ; then
-	# patch libusb to build with android-ndk
-	patch -p1 < libusb-1.0.9-android.patch  libusb-1.0.9/libusb/io.c
-fi
+
 if [ ! -e $PKG_CONFIG_PATH/libusb-1.0.pc ] ; then
 	mkdir -p libusb-build-$ARCH
 	pushd libusb-build-$ARCH
-	../libusb-1.0.9/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared
+	../libusb/configure --host=${BUILDCHAIN} --prefix=${PREFIX} --enable-static --disable-shared --disable-udev
 	make
 	make install
 	popd
